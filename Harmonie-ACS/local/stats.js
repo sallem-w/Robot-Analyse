586846807933ff34eb1867cf8601107bf17365f4
Maxime Richard <richard.maxime9@gmail.com>
2017-04-24 15:35:57 +0200

    add file stats

---
+++ Harmonie-ACS/local/stats.js
@@ -0,0 +1,25 @@
+﻿ctx.stats = (function() {
+	
+	var _fileName = ctx.date.formatYYYMMDD(new Date()) + '_{0}_Stats.html';
+	var stats = {};
+	var _pathFileStats;
+
+	stats.initFileStats = function(pathDirectory, nameScenario) {
+		var pathFile = pathDirectory + fileName.replace('{0}', nameScenario);
+		if (!ctx.fso.file.exist(pathFile)) {
+			ctx.fso.file.create(pathFile);
+		}
+
+		pathFileStats = pathFile;
+	};
+	
+	stats.write = function() {
+		if (str.length === 0) {
+			return;
+		}
+
+		ctx.fso.file.write(pathFileTrace, "");
+	};
+	
+	return stats;
+}) ();
