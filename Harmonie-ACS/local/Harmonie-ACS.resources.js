3c6cd008016cf5eba552c4d71a476af6ac2a826d
Maxime Richard <richard.maxime9@gmail.com>
2017-04-25 11:36:29 +0200

    add page dashboard

--- Harmonie-ACS/local/Harmonie-ACS.resources.js
+++ Harmonie-ACS/local/Harmonie-ACS.resources.js
@@ -4,3 +4,6 @@
 
 
 var ActivInfinite = ctx.addApplication('ActivInfinite', {"nature":"WEB3","path":"http://infinite-haur05/WebAneto/ValiderIdentification.do#"});
+
+ActivInfinite.pDashboard = ActivInfinite.addPage('pDashboard', {"comment":"Activ Infinite - Site 01 - SAPCTOR1 (Utilisateur)","path":"http://infinite-haur05/WebAneto/ValiderIdentification.do"});
+ActivInfinite.pDashboard.btMenu = ActivInfinite.pDashboard.addItem('btMenu');
