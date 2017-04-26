e129251777da6a2cbe795f9cba3109faaef1ddef
Maxime Richard <richard.maxime9@gmail.com>
2017-04-26 10:56:39 +0200

    move page order

--- Harmonie-ACS/local/Harmonie-ACS.resources.js
+++ Harmonie-ACS/local/Harmonie-ACS.resources.js
@@ -5,13 +5,13 @@
 
 var ActivInfinite = ctx.addApplication('ActivInfinite', {"nature":"WEB3","path":"http://infinite-haur05/WebAneto/ValiderIdentification.do#"});
 
+ActivInfinite.pContratIndivFound = ActivInfinite.addPage('pContratIndivFound', {"comment":"[frameInitial0] - Web Aneto","path":"http://infinite-haur05/WebAneto/contrat/ACIC.do?method=rechercher\u0026CONTAINER_NOM_SCENARIO_ENREGISTREMENT=ACCO03STSO_59\u0026CONTAINER_NOM_FONCTION=ACIC_100\u0026AFFICHAGE_CONTAINER="});
+ActivInfinite.pContratIndivFound.oNumeroContrat2 = ActivInfinite.pContratIndivFound.addItem('oNumeroContrat2', {"mustExist":true});
+ActivInfinite.pContratIndivFound.oBtSuivant = ActivInfinite.pContratIndivFound.addItem('oBtSuivant');
+
 ActivInfinite.pDashboard = ActivInfinite.addPage('pDashboard', {"comment":"Activ Infinite - Site 01 - SAPCTOR1 (Utilisateur)","path":"http://infinite-haur05/WebAneto/ValiderIdentification.do"});
 ActivInfinite.pDashboard.btMenu = ActivInfinite.pDashboard.addItem('btMenu');
 
 ActivInfinite.pConsultContratIndiv = ActivInfinite.addPage('pConsultContratIndiv', {"comment":"[frameInitial0] - Web Aneto","path":"http://infinite-haur05/WebAneto/ValiderIdentification.do#"});
 ActivInfinite.pConsultContratIndiv.oNumeroContrat = ActivInfinite.pConsultContratIndiv.addItem('oNumeroContrat');
 ActivInfinite.pConsultContratIndiv.btBtRecherche = ActivInfinite.pConsultContratIndiv.addItem('btBtRecherche');
-
-ActivInfinite.pContratIndivFound = ActivInfinite.addPage('pContratIndivFound', {"comment":"[frameInitial0] - Web Aneto","path":"http://infinite-haur05/WebAneto/contrat/ACIC.do?method=rechercher\u0026CONTAINER_NOM_SCENARIO_ENREGISTREMENT=ACCO03STSO_59\u0026CONTAINER_NOM_FONCTION=ACIC_100\u0026AFFICHAGE_CONTAINER="});
-ActivInfinite.pContratIndivFound.oNumeroContrat2 = ActivInfinite.pContratIndivFound.addItem('oNumeroContrat2', {"mustExist":true});
-ActivInfinite.pContratIndivFound.oBtSuivant = ActivInfinite.pContratIndivFound.addItem('oBtSuivant');
