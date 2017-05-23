9ed5ba24e211e77a5599142da93500a426423c80
Maxime Richard <richard.maxime9@gmail.com>
2017-05-23 16:54:54 +0200

    add helper string + padleft function

---
+++ Harmonie-ACS/local/stringhelper.js
@@ -0,0 +1,10 @@
+﻿ ctx.stringHelper = (function() {
+	
+	var stringHelper = {};
+	
+	stringHelper.padLeft = function(str, paddingValue) {
+		return String(paddingValue + str).slice(-paddingValue.length);
+	};
+
+	return stringHelper;
+}) ();
