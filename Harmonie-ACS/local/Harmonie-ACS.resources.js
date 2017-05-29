19cf0feadbe5cb60d4c88ec7fa841f876f2c2c4f
Maxime Richard <richard.maxime9@gmail.com>
2017-05-29 18:11:14 +0200

    add new page + paste code v6 synthesis

--- Harmonie-ACS/local/Harmonie-ACS.resources.js
+++ Harmonie-ACS/local/Harmonie-ACS.resources.js
@@ -140,3 +140,11 @@ ActivInfinitev7.pDashboard.oMenuHidden = ActivInfinitev7.pDashboard.addItem('oMe
 
 ActivInfinitev7.pConsultContratIndiv = ActivInfinitev7.addPage('pConsultContratIndiv', {"comment":"Consultation - haum0a - Activ Infinite - Module de gestion","path":"http://infinite-haum0a/mdg/Go.do?id=ACCO03STSO"});
 ActivInfinitev7.pConsultContratIndiv.oTitlePage = ActivInfinitev7.pConsultContratIndiv.addItem('oTitlePage', {"mustExist":true});
+
+ActivInfinitev7.pSynthesis = ActivInfinitev7.addPage('pSynthesis', {"comment":"Contexte Contrat - haum0a - Activ Infinite - Module de gestion","path":"http://infinite-haum0a/mdg/AccueilMenu.do?method=rechercher"});
+ActivInfinitev7.pSynthesis.oTitlePage = ActivInfinitev7.pSynthesis.addItem('oTitlePage', {"mustExist":true});
+ActivInfinitev7.pSynthesis.btSearch = ActivInfinitev7.pSynthesis.addItem('btSearch');
+ActivInfinitev7.pSynthesis.oBenefIdentification = ActivInfinitev7.pSynthesis.addItem('oBenefIdentification');
+ActivInfinitev7.pSynthesis.oTypeIdentification = ActivInfinitev7.pSynthesis.addItem('oTypeIdentification');
+ActivInfinitev7.pSynthesis.oDateEnd = ActivInfinitev7.pSynthesis.addItem('oDateEnd', {"occurs":1});
+ActivInfinitev7.pSynthesis.oIndividualContract = ActivInfinitev7.pSynthesis.addItem('oIndividualContract', {"occurs":1});
