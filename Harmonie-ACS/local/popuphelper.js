8fdf507b6bc0e6616bd75b15655f3bcdc9450cfe
Dylan Demougin <dylan@marmelab.com>
2017-06-14 16:22:44 +0200

    fix indent

--- Harmonie-ACS/local/popuphelper.js
+++ Harmonie-ACS/local/popuphelper.js
@@ -5,12 +5,12 @@
 	popupHelper.newPopup = function(messageStr, title) {
 		title = title || 'Erreur';
 		var popup = ctx.popup('pClose').init({
-	  	template: e.popup.template.Ok,
-	 		title: title,
-	  	CX: 500,
-	  	CY: 180,
-	  	message: messageStr, 
-	  	icon: e.popup.icon64.hello
+			template: e.popup.template.Ok,
+			title: title,
+			CX: 500,
+			CY: 180,
+			message: messageStr,
+			icon: e.popup.icon64.hello
 		});
 		popup.open();
 	};
