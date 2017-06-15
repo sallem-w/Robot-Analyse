60f6effeab5a50e0a87d6fd621806e24acdb49c0
Dylan Demougin <dylan@marmelab.com>
2017-06-15 15:59:23 +0200

    fix missing endStep

--- Harmonie-ACS/local/terminatedcmu.js
+++ Harmonie-ACS/local/terminatedcmu.js
@@ -42,4 +42,5 @@ ActivInfinitev7.step({ searchTerminatedContractCMU: function(ev, sc, st) {
 
 ActivInfinitev7.step({ endTerminatedCMU: function(ev, sc, st) {
 	ctx.trace.writeInfo(sc.data.contract.individualContract + ' - STEP END - product terminated CMU');
+	sc.endStep();
 }});
