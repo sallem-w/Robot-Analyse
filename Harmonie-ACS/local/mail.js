5e2480c7add15f359c0a5f51bd53fe8a739d3274
Dylan Demougin <dylan@marmelab.com>
2017-06-28 12:26:17 +0200

    add method for search mail template with path and scenario code

---
+++ Harmonie-ACS/local/mail.js
@@ -0,0 +1,23 @@
+﻿ctx.mail = (function() {
+	var mail = {};
+	var templatePath;
+	
+	mail.init = function(codeScenario) {
+		var pathTemplate = ctx.config.getPathTemplate();
+		var f = ctx.fso.folder.getFileCollection('C:\\Users\\DEMOUGIN-D\\Projet\\Contextor\\template');//pathTemplate);
+		for (; !f.atEnd(); f.moveNext()) {
+			var file = f.item();
+			var filename = file.Name;
+			if (filename.toLowerCase().indexOf(codeScenario.toLowerCase()) !== -1 && filename.toLowerCase().indexOf('.docx') !== -1) {
+				templatePath = pathTemplate + filename;
+				return templatePath;
+			}
+		}
+		
+		//If we are here, it seems that we don't found the template file
+		ctx.popupHelper.newPopup('Aucun fichier word correspondant au template '+ codeScenario +' n\'a été trouvé au chemin suivant : ' + pathTemplate, 'Template introuvable');
+		throw new Error('No template file found');
+	};
+
+	return mail;
+}) ();
