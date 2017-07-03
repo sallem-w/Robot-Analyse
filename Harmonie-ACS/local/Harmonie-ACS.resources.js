b2743d4c39d3386e948223925d4e37620a4e47a9
Maxime Richard <richard.maxime9@gmail.com>
2017-07-03 15:37:01 +0200

    add focus synthsis page

--- Harmonie-ACS/local/Harmonie-ACS.resources.js
+++ Harmonie-ACS/local/Harmonie-ACS.resources.js
@@ -12,8 +12,8 @@ ActivInfinitev7.pSynthesisSearch = ActivInfinitev7.addPage('pSynthesisSearch', {
 ActivInfinitev7.pSynthesisSearch.oTitlePage = ActivInfinitev7.pSynthesisSearch.addItem('oTitlePage', {"mustExist":true});
 ActivInfinitev7.pSynthesisSearch.oTitlePage2 = ActivInfinitev7.pSynthesisSearch.addItem('oTitlePage2', {"mustNotExist":true});
 ActivInfinitev7.pSynthesisSearch.btSearch = ActivInfinitev7.pSynthesisSearch.addItem('btSearch');
-ActivInfinitev7.pSynthesisSearch.oBenefIdentification = ActivInfinitev7.pSynthesisSearch.addItem('oBenefIdentification');
-ActivInfinitev7.pSynthesisSearch.oTypeIdentification = ActivInfinitev7.pSynthesisSearch.addItem('oTypeIdentification');
+ActivInfinitev7.pSynthesisSearch.oBenefIdentification = ActivInfinitev7.pSynthesisSearch.addItem('oBenefIdentification', {"mustExist":true});
+ActivInfinitev7.pSynthesisSearch.oTypeIdentification = ActivInfinitev7.pSynthesisSearch.addItem('oTypeIdentification', {"mustExist":true});
 
 ActivInfinitev7.pSynthesis = ActivInfinitev7.addPage('pSynthesis', {"comment":"Contexte Contrat - haum0a - Activ Infinite - Module de gestion","path":"http://infinite-haum0a/mdg/AccueilMenu.do?method=rechercher"});
 ActivInfinitev7.pSynthesis.oTitlePage = ActivInfinitev7.pSynthesis.addItem('oTitlePage', {"mustExist":true});
@@ -78,6 +78,7 @@ ActivInfinitev7.pProductList.oCodeProduct = ActivInfinitev7.pProductList.addItem
 ActivInfinitev7.pProductList.oEndDateProduct = ActivInfinitev7.pProductList.addItem('oEndDateProduct', {"occurs":1});
 ActivInfinitev7.pProductList.oStateProduct = ActivInfinitev7.pProductList.addItem('oStateProduct', {"occurs":1});
 ActivInfinitev7.pProductList.btVisuCotisation = ActivInfinitev7.pProductList.addItem('btVisuCotisation');
+ActivInfinitev7.pProductList.oProductPaging = ActivInfinitev7.pProductList.addItem('oProductPaging');
 
 ActivInfinitev7.pContribution = ActivInfinitev7.addPage('pContribution', {"comment":"Consultation - N°00248886 - Valide - Adhérent : Monsieur GOURIO LOIC - haum0a - Activ Infinite - Module de gestion","path":"http://infinite-haum0a/mdg/LancementFonction.do?method=debuter\u0026CONTAINER_NOM_SCENARIO_ENREGISTREMENT=ACCO03STSO_114\u0026CONTAINER_NOM_FONCTION=AC36_900\u0026AFFICHAGE_CONTAINER=OK\u0026CONTAINER_FONCTION_FORCAGE_VALIDATION=FALSE\u0026CONTAINER_NOM_SCENARIO_ENREGISTREMENT=ACCO03STSO_114\u0026CONTAINER_NOM_FONCTION=AC3Q_800\u0026AFFICHAGE_CONTAINER"});
 ActivInfinitev7.pContribution.oTitlePage = ActivInfinitev7.pContribution.addItem('oTitlePage', {"mustExist":true});
