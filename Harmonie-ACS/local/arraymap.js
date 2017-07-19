2e8d5105080869f3cf7efd54a46d7cd8ee8c9314
ThieryMichel <thiery@marmelab.com>
2017-07-19 18:16:34 +0200

    refactor checkContractAcs

---
+++ Harmonie-ACS/local/arraymap.js
@@ -0,0 +1,16 @@
+﻿ctx.map = function map(fn, array) {
+	var newArray = [];
+	for(var index = 0; index < array.length; index++) {
+		newArray[index] = fn(array[index], index, array);
+	}
+	
+	return newArray;
+}
+
+ctx.reduce = function reduce(fn, acc, array) {
+	for(var index = 0; index < array.length; index++) {
+		acc = fn(acc, array[index], index, array);
+	}
+	
+	return acc;
+}
