eb11d4e8de47241f740cd72bcb93397f0595e540
ThieryMichel <thiery@marmelab.com>
2017-07-31 18:30:02 +0200

    normalize sirh excel with helper clone, isArray, toArray and join

--- Harmonie-ACS/local/arraymap.js
+++ Harmonie-ACS/local/arraymap.js
@@ -16,3 +16,34 @@ _.reduce = function reduce(fn, acc, array) {
 	
 	return acc;
 }
+
+_.isArray = function (data) {
+    return Object.prototype.toString.call(data) === '[object Array]';
+}
+
+_.toArray = function (data) {
+	if (typeof data !== 'object') {
+		throw new Error(JSON.stringify(data) + ' : Cannot be converted to array');
+	}
+	if(_.isArray(data)) {
+		return data;
+	}
+	return _.map(function (key, index) {
+		ctx.trace.writeInfo('key ' + key);
+		ctx.trace.writeInfo('index ' + index);
+		if (isNaN(parseInt(key))) {
+			throw new Error(JSON.stringify(data) + ' : Cannot be converted to array, key : ' + key + ' is not a number');
+		}
+		return data[key];
+	}, Object.keys(data));
+}
+
+_.join = function join(array, delimiter) {
+	delimiter = delimiter || ',';
+	return _.reduce(function (str, item) {
+		if (!str) {
+			return item;
+		}
+		return str + delimiter +  item;
+	}, null, array);
+}
